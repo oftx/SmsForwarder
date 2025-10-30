0736ca1419fa6855a0c67f0c642e3455a8508d37
oftx <52040558+oftx@users.noreply.github.com>
2025-10-30 18:55:13 +0800

    chore: Bump version to 1.6
    Updates the application version:
    *   `versionCode` from 6 to 7
    *   `versionName` from "1.5" to "1.6"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "github.oftx.smsforwarder"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = 7
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
